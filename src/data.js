28dba8840aa711a84f4f5ab65182676eee7c3efc
Brendon <brendonnewton0@gmail.com>
2024-05-16 16:28:43 -0500

    Adding sample data with a Task component

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const data = [
+    // Sample tasks with a title and description
+    { title: 'Task 1', description: 'This is the first task' },
+    { title: 'Task 2', description: 'This is the second task' },
+    { title: 'Task 3', description: 'This is the third task' },
+];
---
+++ src/task.js
@@ -0,0 +1,10 @@
+const Task = ({title, description}) => {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    );
+}
+
+export default Task;
